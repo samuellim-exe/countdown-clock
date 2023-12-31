b71254697587552e11dad3a1bb80d421f4aa2ad7
Samuel Lim <65169348+samuellim-exe@users.noreply.github.com>
2023-12-31 11:55:14 +0000

    okok

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.redirect('/index.html');
 });
 
 app.listen(port, () => { 
